7a522d7e5a67d897b13c5fe208401d5bb931080c
Sourav kumar jha <souravkumarjha@Souravs-MacBook-Air.local>
2026-01-09 14:04:22 -0500

    Auth form schema done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import {
   Form,
   FormControl,
@@ -13,49 +14,70 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(2, "Username must be at least 2 characters").max(50),
-})
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
 
-const AuthForm = () => {
+const AuthForm = ({ type }: { type: FormType }) => {
+  const formSchema = authFormSchema(type);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
-  })
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+    console.log(values);
   }
+
+  const isSignIn = type === "sign-in";
 
   return (
     <div className="card-border lg:min-w-[566px]">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>This is your public display name.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+      <div className="flex flex-col gap- card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">InterviewPulse</h2>
+        </div>
+        <h3>Practice job interview with AI</h3>
 
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignIn && <p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign in" : "Create an Account"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSignIn ? "No account yet ? " : "Have an account already ?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign in" : "Sign up"}
+          </Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
