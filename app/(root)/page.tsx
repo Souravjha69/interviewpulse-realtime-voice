3ebdf21a7b18334fb0f9135a2ecc1c2d66acb35b
Sourav kumar jha <souravkumarjha@Souravs-MacBook-Air.local>
2026-01-27 23:23:14 -0500

    57:00 Home page creation is going on

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+    <section className='card-cta'>
+      <div className='flex flex-col gap-6 max-w-lg'>
+        <h2>Get Interview-Ready with AI-Powered Practice and Feedback</h2>
+        <p className='text-lg'>
+          Practice on real interview questions & get instant feedback
+        </p>
+        <Button asChild className="btn-primary max-sm:w-full">
+          <Link href="/interview">Start an Inteview</Link>
+        </Button>
+      </div>
+      <div className="hidden sm:block">
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="object-contain"
+        />
+      </div>
+    </section>
+    <section className='flex flex-col gap-6 mt-8'>
+      <h2>Your Interviews</h2>
+      <div className='interviews-section'>
+        <p>You haven't taken any interviews yet.</p>
+      </div>
+    </section>
+    </>
   )
 }
 
