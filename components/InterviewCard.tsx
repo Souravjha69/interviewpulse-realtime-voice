be0aa69694e0b158e8704266047205d19a0dac8e
Sourav kumar jha <souravkumarjha@Souravs-MacBook-Air.local>
2026-01-29 16:41:03 -0500

    01:11:46 right now button is creating and UI is fixed here Your Intervierws here

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import dayjs from "dayjs";
 import { interviewCovers } from "@/constants";
 import { getRandomInterviewCover } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 
 
@@ -15,47 +16,54 @@ const InterviewCard = ({ role, type, techstack, createdAt }: InterviewCardProps)
   );
 
   return (
-    <div className="card-border w-[360px] max-sm:w-full min-h-96">
-      <div className="card-interview">
-        <div>
-          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
-            <p className="badge-text">{normalizedType}</p>
-          </div>
-
-          <Image
-            src={getRandomInterviewCover()}
-            alt="coverimage"
-            width={90}
-            height={90}
-            className="rounded-full object-cover size-[90px]"
-          />
-        </div>
-
-        <h3 className="mt-5 capitalize">
-            {role} Interview
-        </h3>
-
-        <div className="flex flex-row gap-5 mt-3">
-            <div className="flex flex-row gap-2">
-                <Image src="/calendar.svg" alt="calender" width={22} height={22}/>
-                <p>{formattedDate}</p>
+    <>
+      <div className="card-border w-[360px] max-sm:w-full min-h-96">
+        <div className="card-interview">
+          <div>
+            <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+              <p className="badge-text">{normalizedType}</p>
             </div>
-        </div>
 
-
-        {/* <div className="flex flex-col gap-2">
-          <h3 className="text-2xl font-semibold">{role}</h3>
-          <p className="text-sm text-primary-100">{formattedDate}</p>
-          <div className="flex flex-wrap gap-2">
-            {techstack?.slice(0, 4).map((tech) => (
-              <span key={tech} className="text-xs px-2 py-1 rounded-full bg-light-600">
-                {tech}
-              </span>
-            ))}
+            <Image
+              src={getRandomInterviewCover()}
+              alt="coverimage"
+              width={90}
+              height={90}
+              className="rounded-full object-cover size-[90px]"
+            />
           </div>
-        </div> */}
+
+          <h3 className="mt-5 capitalize">
+            {role} Interview
+          </h3>
+
+          <div className="flex flex-row gap-5 mt-3">
+            <div className="flex flex-row gap-2">
+              <Image src="/calendar.svg" alt="calender" width={22} height={22} />
+              <p>{formattedDate}</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-2 items-center">
+            <Image src="/star.svg" alt="star" width={22} height={22} />
+            <p>{feedback?.totalScore || "---"}/100</p>
+          </div>
+          <div className="mt-auto flex flex-col gap-4">
+            <p className="line-clamp-2">
+              {feedback?.finalAssessment ||
+                "You haven't taken the interview yet. Take it now to improve the skills."}
+            </p>
+            <div className="flex flex-row justify-between">
+              <p>Tech Icons</p>
+              <Button className="bt-primary">
+
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      
+    </>
   );
 };
 
